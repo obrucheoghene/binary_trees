dbc5a84930c346578b1132337fd8f026857bf793
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-30 18:48:04 +0100

    Update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,12 +9,14 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *leftNode;
+
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
 
-	binary_tree_t *leftNode = malloc(sizeof(binary_tree_t));
+	leftNode = malloc(sizeof(binary_tree_t));
 
 	if (leftNode == NULL)
 	{
