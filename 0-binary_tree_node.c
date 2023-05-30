07d8aa5096708dd512b527035994f51e497907f1
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-30 17:50:22 +0100

    Fix requirement

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,23 +4,21 @@
  * binary_tree_node - Creates a binary tree node
  * @parent: Pointer to parent node
  * @value: new node value
- * 
  * Return: Binary tree node
 */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t * newNode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
-    if (newNode == NULL)
-    {
-        return NULL;
-    }
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
 
-    newNode->n = value;
-    newNode->parent = parent;
-    newNode->left = NULL;
-    newNode->right = NULL;
-
-    return newNode;
+	return (newNode);
 }
