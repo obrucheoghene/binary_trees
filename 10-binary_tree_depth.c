07491282982eb7dbdbe342031e4f3b7ea9ca3c78
tonytec <tonyexplorer202@gmail.com>
2023-05-31 17:39:34 +0100

    Update 9-binary_tree_height.c file

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_depth - Measures the depth of a node in a binary tree
+ * @tree: the root of the tree to measure
+ *
+ * Return: return the dept of the tree else 0
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return (0);
+
+    
+}
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+size_t max_tree_height(const binary_tree_t *tree);
+
 /**
  * binary_tree_height - Measures the height of a binary tree
  * @tree: the root of the tree
@@ -9,15 +11,32 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height = 0;
-	int right_height = 0;
+	size_t max_height;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left) + 1;
-	right_height = binary_tree_height(tree->right) + 1;
+	max_height = max_tree_height(tree);
+	return (max_height - 1);
+}
 
-	return (left_height > right_height ? left_height - 1 : right_height - 1);
+/**
+ * max_tree_height - Measure the max height of the subtree
+ * @tree: the root of the tree
+ *
+ * Return: return the max subtree
+*/
 
+size_t max_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = max_tree_height(tree->left) + 1;
+	right_height = max_tree_height(tree->right) + 1;
+
+	return (left_height > right_height ? left_height : right_height);
 }
