19e381dc2d44fdbb7e194f3e9e4ca116ebb24f5a
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-31 15:01:25 +0100

    Add 2-binary

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,22 +17,17 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node =  binary_tree_node(parent, value);
 
 	if (new_node == NULL)
 		return (NULL);
-
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->right = NULL;
-	new_node->left = NULL;
 
 	if (parent->right == NULL)
 		parent->right = new_node;
 	else
 	{
 		new_node->right = parent->right;
-		parent->right = new_node;
+		parent->right->parent = new_node;
 	}
 
 	return (new_node);
