cb4721cb28e65dd759c8dacd9f6b216c2b07b5d7
tonytec <tonyexplorer202@gmail.com>
2023-06-01 00:42:32 +0100

    Add 11-binary_tree_size.c file

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+size_t tree_size(const binary_tree_t *tree);
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: the root of the tree to measure
+ *
+ * Return: return the size of the tree else 0
+*/
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (tree_size(tree));
+}
+
+/**
+ * tree_size - Measures the size of a binary tree
+ * @tree: the root of the tree to measure
+ *
+ * Return: return the size of the tree else 0
+*/
+
+size_t tree_size(const binary_tree_t *tree)
+{
+	size_t size;
+
+	if (tree == NULL)
+		return (0);
+
+	size = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+
+	return (size);
+}
