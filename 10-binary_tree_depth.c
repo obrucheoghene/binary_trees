a34f04a9165a8c2e44a9d67f331d964e702bcff7
tonytec <tonyexplorer202@gmail.com>
2023-06-01 00:26:39 +0100

    Add 10-binary_tree_depth.c file

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+size_t max_depth(const binary_tree_t *tree);
 
 /**
  * binary_tree_depth - Measures the depth of a node in a binary tree
@@ -7,10 +8,30 @@
  *
  * Return: return the dept of the tree else 0
 */
+
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    
+	return (max_depth(tree) - 1);
+}
+
+/**
+ * max_depth - Measures the depth of a node in a binary tree
+ * @tree: the root of the tree to measure
+ *
+ * Return: return the dept of the tree else 0
+*/
+
+size_t max_depth(const binary_tree_t *tree)
+{
+	size_t mx;
+
+	if (tree == NULL)
+		return (0);
+
+	mx = max_depth(tree->parent) + 1;
+
+	return (mx);
 }
