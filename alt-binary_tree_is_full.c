f1d018050ed7b4c1e1efbfd79532ea01b266d293
tonytec <tonyexplorer202@gmail.com>
2023-06-02 04:35:27 +0100

    Merge upstream
    Add 15-binary_tree_is_full.c alternative

---
+++ alt-binary_tree_is_full.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_is_full - Checks if a binary tree is full
+ * @tree: the root of the tree
+ *
+ * Return: return 1 if the tree is full else 0
+*/
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if ((tree->left) && (tree->right))
+		return ((binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right)) ? 1 : 0);
+	return (0);
+}
